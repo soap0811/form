a70c4278203e322ce88b8585ca1e1eb1d642aead
soap0811 <soap0811@ukr.net>
2019-07-01 17:05:48 +0300

    fourth

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <form action="form.php" method="POST">
+        <div class="row">
+          <div class="col">
+            <input type="text" class="form-control" placeholder="First name" name="first_name" >
+          </div>
+          <div class="col">
+            <input type="text" class="form-control" placeholder="Last name" name="last_name">
+            <button type="submit" class="btn btn-primary mb-2">Submit</button>
+          </div>
+        </div>
+      </form>
+
+</body>
+</html>
