623d53bc52cc52e61bf9b37a13033aa3c8e62453
soap0811 <soap0811@ukr.net>
2019-07-01 17:38:47 +0300

    seven

--- form.php
+++ form.php
@@ -1,2 +1,5 @@
 
-Ваше имя<?php echo htmlspecialchars($_POST['first_name']);?>
+<?php echo 'Ваше имя' htmlspecialchars($_POST['first_name']);
+
+echo 'Ваша фамилия' htmlspecialchars($_POST['last_name']);
+?>
